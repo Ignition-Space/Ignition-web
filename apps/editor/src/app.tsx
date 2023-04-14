702739a061a331e131b2c784c87ef4213d46802f
wangly19 <18607948581@163.com>
2023-04-14 08:56:45 -0700

    feat(editor): 添加编辑器相关项目

---
+++ apps/editor/src/app.tsx
@@ -0,0 +1,15 @@
+import { ConfigProvider, theme } from 'antd';
+import 'antd/dist/reset.css'
+
+export function rootContainer(container: any) {
+	return (
+		<ConfigProvider theme={{
+			token: {
+				colorBorderSecondary: '#eaeaeb',
+				colorFillAlter: '#f6f8fa'
+			}
+		}} >
+			{container}
+		</ConfigProvider>
+	)
+}
