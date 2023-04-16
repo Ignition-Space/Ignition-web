5a235c447c171533105be93169061a7683fad546
wangly19 <18607948581@163.com>
2023-04-16 23:43:46 +0800

    feat(core): 物料组件创建HOC和方法

---
+++ packages/core/src/create.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import {useNode, UserComponent, UserComponentConfig} from '@craftjs/core'
+
+export type MaterialComponent = UserComponent
+
+export function withMaterialNode<T = any>(WrapComponent: React.FunctionComponent<T>) {
+    return function (props: any) {
+        const { connectors: {connect, drag} } = useNode();
+
+        return <WrapComponent ref={(dom: HTMLElement) => connect(drag(dom))} {...props} />
+    }
+}
+
+export function createReactMaterial<T>(component: MaterialComponent, options: UserComponentConfig<T>) {
+    component.craft = options
+    return component as MaterialComponent
+}
