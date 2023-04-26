30829a79fb54c37617cc7da94f3a651f4bc1c26a
wangly19 <18607948581@163.com>
2023-04-26 23:54:18 +0800

    feat(config): 优化相关配置

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,28 +1,33 @@
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 
+const externalPackages = [
+  "@craftjs/core",
+  'react', 'react-dom', 'antd'
+]
+
 export default {
-    plugins: [
-        react(),
-        dts()],
-    build: {
-        lib: {
-            entry: 'src/index.ts',
-            name: 'lgnition.core',
-            fileName: (format) => `index.${format}.js`
+  plugins: [
+    react(),
+    dts()],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'lgnition.core',
+      fileName: (format) => `index.${format}.js`
+    },
+    rollupOptions: {
+      // 排除 React 和 React DOM，因为这些库应该由使用你的库的项目来提供
+      external: externalPackages,
+      output: {
+        // 配置 UMD 格式，使你的组件库可以在不同的环境中使用
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
         },
-        rollupOptions: {
-            // 排除 React 和 React DOM，因为这些库应该由使用你的库的项目来提供
-            external: ['react', 'react-dom'],
-            output: {
-                // 配置 UMD 格式，使你的组件库可以在不同的环境中使用
-                globals: {
-                    react: 'React',
-                    'react-dom': 'ReactDOM'
-                },
-                // 配置 minify 选项，使输出文件更小
-                minifyInternalExports: true
-            }
-        }
+        // 配置 minify 选项，使输出文件更小
+        minifyInternalExports: true
+      }
     }
-};
+  }
+}
