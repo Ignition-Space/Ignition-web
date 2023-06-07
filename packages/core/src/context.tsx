ad65d3de220cb3493c76f6c3c2728bcb65ecbf08
wangly19 <18607948581@163.com>
2023-06-07 23:22:37 +0800

    feat(setter): 完善ColorPickerSetter组件

--- packages/core/src/context.tsx
+++ packages/core/src/context.tsx
@@ -3,7 +3,10 @@ import * as React from 'react'
 export interface FrameworkProviderProps {
   children?: React.ReactNode;
   enable?: boolean;
+  schema?: string;
 }
 
-export const FrameworkContext = React.createContext<FrameworkProviderProps | null>(null);
+export const FrameworkContext = React.createContext<FrameworkProviderProps | null>({
+  enable: true
+});
 
