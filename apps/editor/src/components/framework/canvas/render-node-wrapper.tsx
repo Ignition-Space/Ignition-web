2ca784aac98fbc6b80d7ace421bc3037c64bc621
wangly19 <18607948581@163.com>
2023-06-20 23:54:26 +0800

    chore(editor): 删除节点沉淀代码

--- apps/editor/src/components/framework/canvas/render-node-wrapper.tsx
+++ apps/editor/src/components/framework/canvas/render-node-wrapper.tsx
@@ -1,20 +1,29 @@
 import * as React from 'react'
 import { useNode, useEditor } from '@craftjs/core'
-import { theme } from 'antd'
-import { css } from '@emotion/css'
-import { CacheProvider } from '@emotion/react'
-import createCache from '@emotion/cache'
+import type { MenuProps } from 'antd';
+import { Dropdown } from 'antd'
+
+const items: MenuProps['items'] = [
+  {
+    label: '1st menu item',
+    key: '1',
+  },
+  {
+    label: '2nd menu item',
+    key: '2',
+  },
+  {
+    label: '3rd menu item',
+    key: '3',
+  },
+];
 
 export interface RenderNodeWrapperProps {
     render: React.ReactElement
 }
 
-const ViewPortID = '#__CasterViewPort__'
 
 export const RenderNodeWrapper: React.FC<RenderNodeWrapperProps> = ({ render }) => {
-  const currentRef = React.useRef<HTMLDivElement>(null)
-  const { token } = theme.useToken()
-  const [cache, setCache] = React.useState<any>()
 
   const { id } = useNode()
   const { query, isActive, isHovered } = useEditor((_, queryEditor) => ({
@@ -55,34 +64,6 @@ export const RenderNodeWrapper: React.FC<RenderNodeWrapperProps> = ({ render }) 
     }
   }, [dom, isHovered, isRootNode, isActive])
 
-  const getPos = React.useCallback((curDOM: HTMLElement | null) => {
-    const { top, left, bottom } = curDOM
-      ? curDOM.getBoundingClientRect()
-      : { top: 0, left: 0, bottom: 0 }
-    return {
-      top: `${top > 0 ? top : bottom}px`,
-      left: `${left}px`
-    }
-  }, [])
-
-  const scroll = React.useCallback(() => {
-    const { current: currentDOM } = currentRef
-
-    if (!currentDOM) return
-    const { top, left } = getPos(dom as HTMLElement)
-    currentDOM.style.top = top
-    currentDOM.style.left = left
-  }, [dom, getPos])
-
-  React.useEffect(() => {
-    const mountDocument = document?.querySelector(ViewPortID)
-    mountDocument?.addEventListener('scroll', scroll)
-
-    return () => {
-      mountDocument
-        ?.removeEventListener('scroll', scroll)
-    }
-  }, [scroll])
   return (
     <>
       {render}
