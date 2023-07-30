0bf17423bb829ffee96f0b0721dbe4ac9993c5b8
wangly19 <18607948581@163.com>
2023-07-30 23:45:03 +0800

    feat(materials): 表单控件物料

---
+++ packages/materials/src/form/control/index.tsx
@@ -0,0 +1,36 @@
+import type * as React from 'react'
+import { createReactMaterial, withMaterialNode } from '@lgnition-lowcode/core'
+import TitleSettings from './settings'
+import { SliderView, SwitchView, CheckboxView, SegmentedView } from './view'
+
+export const Segmented = createReactMaterial(withMaterialNode<React.ComponentProps<typeof SegmentedView>>(SegmentedView), {
+  displayName: 'Segmented',
+  related: {
+    settingRender: TitleSettings
+  },
+})
+
+
+export const Checkbox= createReactMaterial(withMaterialNode<React.ComponentProps<typeof CheckboxView>>(CheckboxView), {
+  displayName: 'Checkbox',
+  related: {
+    settingRender: TitleSettings
+  },
+})
+
+
+
+export const Switch = createReactMaterial(withMaterialNode<React.ComponentProps<typeof SwitchView>>(SwitchView), {
+  displayName: 'Switch',
+  related: {
+    settingRender: TitleSettings
+  },
+})
+
+
+export const Slider = createReactMaterial(withMaterialNode<React.ComponentProps<typeof SliderView>>(SliderView), {
+  displayName: 'Slider',
+  related: {
+    settingRender: TitleSettings
+  },
+})
---
+++ packages/materials/src/form/control/view.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { ProFormCheckbox, ProFormSlider, ProFormSwitch, ProFormSegmented } from "@ant-design/pro-components";
+
+export const SegmentedView = React.forwardRef<
+  HTMLDivElement,
+  React.ComponentProps<typeof ProFormSegmented>
+>((props, ref) => {
+  return (
+    <div ref={ref}>
+      <ProFormSegmented {...props} />
+    </div>
+  );
+});
+
+export const SwitchView = React.forwardRef<
+  HTMLDivElement,
+  React.ComponentProps<typeof ProFormSwitch>
+>((props, ref) => {
+  return (
+    <div ref={ref}>
+      <ProFormSwitch {...props} />
+    </div>
+  );
+});
+
+export const SliderView = React.forwardRef<
+  HTMLDivElement,
+  React.ComponentProps<typeof ProFormSlider>
+>((props, ref) => {
+  return (
+    <div ref={ref}>
+      <ProFormSlider {...props} />
+    </div>
+  );
+});
+
+
+export const CheckboxView = React.forwardRef<
+  HTMLDivElement,
+  React.ComponentProps<typeof ProFormCheckbox>
+>((props, ref) => {
+  return (
+    <div ref={ref}>
+      <ProFormCheckbox {...props} />
+    </div>
+  );
+});
