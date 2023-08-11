67ca08f93ea0ba33c994b131b28c054f64ee8f61
wangly19 <18607948581@163.com>
2023-08-11 23:57:31 +0800

    feat(icons): 新增icons图标包，基于remix-icons

---
+++ packages/icons/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup'
+
+
+export default defineConfig((options) => ({
+  minify: !options.watch,
+  entry: ['src/index.ts'],
+  splitting: false,
+  sourcemap: false,
+  clean: false,
+  format: ['cjs', 'esm', 'iife'],
+  external: ["react", "react-dom", "antd", "@ant-design/icons"],
+  dts: true
+}))
