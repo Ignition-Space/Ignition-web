333c903ceb7776a8686d9c6e392eb65ce35ece7c
wangly19 <18607948581@163.com>
2023-08-16 23:59:37 +0800

    chore(editor): 更新mainjs

--- apps/editor/src/main.tsx
+++ apps/editor/src/main.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { ConfigProvider } from 'antd'
-import router from './router'
-import 'antd/dist/reset.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { ConfigProvider } from "antd";
+import router from "./router";
+import "antd/dist/reset.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const token = {
+  borderRadius: 3,
+  wireframe: false,
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ConfigProvider>
+    <ConfigProvider
+      theme={{
+        token,
+      }}
+    >
       <RouterProvider router={router} />
     </ConfigProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
