39d9fc0ac90f84555f0611fc7358b4f7c8c30232
wangly19 <18607948581@163.com>
2023-08-26 23:51:21 +0800

    chore(main): 主工程入口优化

--- apps/main/src/main.tsx
+++ apps/main/src/main.tsx
@@ -1,16 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ConfigProvider, App } from 'antd'
-import { router } from '@/router'
-import { RouterProvider } from 'react-router-dom'
-import 'antd/dist/reset.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ConfigProvider, App } from "antd";
+import { router } from "@/router";
+import { RouterProvider } from "react-router-dom";
+import type { AliasToken } from "antd/es/theme/internal";
+import "antd/dist/reset.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const token: Partial<AliasToken> = {
+  colorPrimary: "#722ED1",
+  colorInfo: "#722ED1",
+  borderRadius: 4,
+  wireframe: false,
+  colorBorder: "#e5e6eb",
+  colorBorderSecondary: "#e5e6eb",
+  fontWeightStrong: 400
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ConfigProvider>
+    <ConfigProvider
+      theme={{
+        token,
+      }}
+    >
       <App>
         <RouterProvider router={router} />
       </App>
     </ConfigProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
