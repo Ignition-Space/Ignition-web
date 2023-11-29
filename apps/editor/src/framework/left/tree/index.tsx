c481f872101c2e6d1be2f90983d32d7e84569956
wangly19 <18607948581@163.com>
2023-11-29 23:59:27 +0800

    feat(editor): tree组件优化

--- apps/editor/src/framework/left/tree/index.tsx
+++ apps/editor/src/framework/left/tree/index.tsx
@@ -1,9 +1,68 @@
-import { ComponentTree } from './component-tree'
+import React from "react";
+import type { MenuProps } from "antd";
+import { ConfigProvider, Menu } from "antd";
+import { useSchema } from "@/framework/stores/useSchema";
+import { SerializedNodes } from "@craftjs/core";
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+function convertToTree(data: SerializedNodes): MenuItem[] {
+  const rootNode = data["ROOT"];
+  const tree: MenuItem[] = [];
+
+  function traverse(nodeId: string): MenuItem {
+    const node = data[nodeId];
+    const { nodes, displayName } = node;
+
+    const treeItem: MenuItem = {
+      label: displayName,
+      key: nodeId,
+      children: nodes.length > 0 ? nodes.map(traverse) : undefined,
+    };
+
+    return treeItem;
+  }
+
+  if (rootNode) {
+    tree.push(traverse("ROOT"));
+  }
+
+  return tree;
+}
 
 export const Tree = () => {
+  const { serializeNodes } = useSchema();
+
+  const ndoeTreeData = React.useMemo(() => {
+    if (serializeNodes) {
+      console.log(serializeNodes, "serializeNodes");
+      return convertToTree(serializeNodes);
+    }
+    return [];
+  }, [serializeNodes]);
+
+  console.log(ndoeTreeData, "ndoeTreeData");
+
   return (
-    <div>
-      <ComponentTree/>
-    </div>
-  )
-}
+    <ConfigProvider
+      theme={{
+        components: {
+          Menu: {
+            itemHeight: 35,
+            itemBorderRadius: 0,
+            itemMarginBlock: 0,
+            itemMarginInline: 0,
+            activeBarBorderWidth: 0,
+            itemPaddingInline: 0,
+          },
+        },
+      }}
+    >
+      <Menu
+        items={ndoeTreeData}
+        inlineIndent={12}
+        expandIcon={null}
+      />
+    </ConfigProvider>
+  );
+};
