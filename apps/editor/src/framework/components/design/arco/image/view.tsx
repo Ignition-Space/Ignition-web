824df99d3317271b5cb83f0eba9eb9648129daf8
wangly19 <18607948581@163.com>
2024-01-14 23:44:14 +0800

    feat(editor): 移除arco-design组件

--- apps/editor/src/framework/components/design/arco/image/view.tsx
+++
@@ -1,9 +0,0 @@
-import { default as Image, ImageProps } from '@arco-design/web-react/es/Image'
-import { ReactMaterialViewType } from "@huos/core";
-
-export const ImageView: ReactMaterialViewType<ImageProps> = (props, ref: any) => {
-
-  return (
-    <Image ref={ref} {...props} />
-  );
-}
